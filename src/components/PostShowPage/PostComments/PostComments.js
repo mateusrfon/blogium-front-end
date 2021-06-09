65d9875c0e01c6d94ea3de799de6bb6510267eaa
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-09 20:09:11 -0300

    get comments

--- src/components/PostShowPage/PostComments/PostComments.js
+++ src/components/PostShowPage/PostComments/PostComments.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 import Comment from './Comment';
 import AddComment from './AddComment';
@@ -8,17 +9,10 @@ export default function PostComments({ postId }) {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    setComments([{
-      id: 1,
-      postId: postId,
-      author: 'João',
-      content: 'Muito bom esse post! Tá de parabéns'
-    }, {
-      id: 2,
-      postId: postId,
-      author: 'Maria',
-      content: 'Como faz pra dar palmas?'
-    }])
+    const request = axios.get(`http://localhost:4001/posts/${postId}/comments`)
+    request.then(r => {
+      setComments(r.data)
+    });
   }, [postId]);
   
   return (
