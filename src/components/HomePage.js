5ab2e2fe71248a45e89dff48fb93900210f25bff
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-09 18:44:09 -0300

    get posts

---
+++ src/components/HomePage.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import PostList from './PostList/PostList';
+import axios from 'axios';
+
+export default function HomePage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const request = axios.get("http://localhost:4001/posts");
+    request.then(r => {
+      setPosts(r.data);
+    });
+  }, []);
+
+  return (
+    <PostList name="Daily stories" posts={posts} />
+  );
+}
